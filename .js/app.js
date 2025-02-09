c4ff2be838060b569b972783008a97cc31168f5a
Ângelo <98216100+angelodesenvolvedor@users.noreply.github.com>
2025-02-08 21:42:20 -0300

    app

--- .js/app.js
+++ .js/app.js
@@ -1,59 +1,56 @@
-document.getElementById('contactForm').addEventListener('submit', function (event) {
-    document.getElementById('success-message').classList.remove('hidden');
-    setTimeout(function () {
-        document.getElementById('success-message').classList.add('hidden');
-    }, 5000);
-});
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.getElementById('menu-toggle');
     const menu = document.getElementById('menu');
     const menuLinks = menu.querySelectorAll('a');
     let menuTimeout;
-
+  
     function closeMenu() {
-        menu.classList.add('hidden');
-        menu.classList.remove('active');
-        clearTimeout(menuTimeout);
+      menu.classList.add('hidden');
+      menu.classList.remove('active');
+      menuToggle.classList.remove('active');
+      clearTimeout(menuTimeout);
     }
-
-    menuToggle.addEventListener('click', function () {
-        menu.classList.toggle('hidden');
-        menu.classList.toggle('active');
-        clearTimeout(menuTimeout);
-
-        if (menu.classList.contains('active')) {
-            menuTimeout = setTimeout(closeMenu, 5000);
-        }
-    });
-
-    menuLinks.forEach(link => {
-        link.addEventListener('click', function (event) {
-            event.preventDefault();
-            closeMenu();
-            const targetId = this.getAttribute('href');
-            const targetElement = document.querySelector(targetId);
-
-            if (targetElement) {
-                window.scrollTo({
-                    top: targetElement.offsetTop,
-                    behavior: 'smooth'
-                });
-            }
+  
+    function scrollToSection(targetId) {
+      const targetElement = document.querySelector(targetId);
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: 'smooth',
         });
+      }
+    }
+  
+    function checkScreenSize() {
+      if (window.innerWidth >= 768) {
+        menu.classList.remove('hidden', 'active');
+        menuToggle.classList.remove('active');
+      } else if (!menuToggle.classList.contains('active')) {
+        menu.classList.add('hidden');
+      }
+    }
+  
+    menuToggle.addEventListener('click', function () {
+      menu.classList.toggle('hidden');
+      menu.classList.toggle('active');
+      menuToggle.classList.toggle('active');
+      clearTimeout(menuTimeout);
+  
+      if (menu.classList.contains('active')) {
+        menuTimeout = setTimeout(closeMenu, 5000);
+      }
     });
-
-     const desktopMenuLinks = document.querySelectorAll('#menu li a');
-       desktopMenuLinks.forEach(link => {
-           link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const targetId = this.getAttribute('href');
-            const targetElement = document.querySelector(targetId);
-            if(targetElement){
-                window.scrollTo({
-                    top: targetElement.offsetTop,
-                    behavior: 'smooth'
-                });
-            }
-        })
-      })
+  
+    menuLinks.forEach((link) => {
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
+        closeMenu();
+        const targetId = this.getAttribute('href');
+        scrollToSection(targetId);
+      });
+    });
+  
+    checkScreenSize();
+  
+    window.addEventListener('resize', checkScreenSize);
 });
