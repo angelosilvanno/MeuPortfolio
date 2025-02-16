8f20a38c0e28e2934d2b5a8a12739d528f0fd6ed
Ângelo <98216100+angelodesenvolvedor@users.noreply.github.com>
2025-02-15 21:41:56 -0300

    app

--- .js/app.js
+++ .js/app.js
@@ -1,56 +1,59 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.getElementById('menu-toggle');
-    const menu = document.getElementById('menu');
-    const menuLinks = menu.querySelectorAll('a');
-    let menuTimeout;
-  
-    function closeMenu() {
+  const menuToggle = document.getElementById('menu-toggle');
+  const menu = document.getElementById('menu');
+  const menuLinks = menu.querySelectorAll('a');
+  let menuTimeout;
+
+  function closeMenu() {
       menu.classList.add('hidden');
       menu.classList.remove('active');
       menuToggle.classList.remove('active');
       clearTimeout(menuTimeout);
-    }
-  
-    function scrollToSection(targetId) {
+  }
+
+  function scrollToSection(targetId) {
       const targetElement = document.querySelector(targetId);
       if (targetElement) {
-        window.scrollTo({
-          top: targetElement.offsetTop,
-          behavior: 'smooth',
-        });
+          const headerOffset = document.querySelector('header').offsetHeight; // Altura do cabeçalho
+          const elementPosition = targetElement.offsetTop - headerOffset;
+
+          window.scrollTo({
+              top: elementPosition,
+              behavior: 'smooth',
+          });
       }
-    }
-  
-    function checkScreenSize() {
+  }
+
+  function checkScreenSize() {
       if (window.innerWidth >= 768) {
-        menu.classList.remove('hidden', 'active');
-        menuToggle.classList.remove('active');
+          menu.classList.remove('hidden', 'active');
+          menuToggle.classList.remove('active');
       } else if (!menuToggle.classList.contains('active')) {
-        menu.classList.add('hidden');
+          menu.classList.add('hidden');
       }
-    }
-  
-    menuToggle.addEventListener('click', function () {
+  }
+
+  menuToggle.addEventListener('click', function () {
       menu.classList.toggle('hidden');
       menu.classList.toggle('active');
       menuToggle.classList.toggle('active');
       clearTimeout(menuTimeout);
-  
+
       if (menu.classList.contains('active')) {
-        menuTimeout = setTimeout(closeMenu, 5000);
+          menuTimeout = setTimeout(closeMenu, 5000);
       }
-    });
-  
-    menuLinks.forEach((link) => {
+  });
+
+  menuLinks.forEach((link) => {
       link.addEventListener('click', function (event) {
-        event.preventDefault();
-        closeMenu();
-        const targetId = this.getAttribute('href');
-        scrollToSection(targetId);
+          event.preventDefault();
+          closeMenu();
+          const targetId = this.getAttribute('href');
+          scrollToSection(targetId);
       });
-    });
-  
-    checkScreenSize();
-  
-    window.addEventListener('resize', checkScreenSize);
+  });
+
+  checkScreenSize();
+
+  window.addEventListener('resize', checkScreenSize);
 });
